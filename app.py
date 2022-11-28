940f4ac48f6361e0dee50bfd408ab02c0a53b4bf
jessjenk1ns <jessjenkins@college.harvard.edu>
2022-11-28 15:30:59 -0500

    hi

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///project.db")
+
+
+# REGISTER
+
+    # Check that email is "---@harvard.college.edu"
+
+    # Return error if username is taken or passwords don't match
+
+    # hash password (finance)
+
+
+# LOGIN
+
+# LOGOUT
