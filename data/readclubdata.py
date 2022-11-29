65ff17a1564259a8da6773f8de91873b2ebbbcb3
John Russell <johnandrewrussell@gmail.com>
2022-11-29 18:04:29 -0500

    update

--- data/readclubdata.py
+++ data/readclubdata.py
@@ -5,8 +5,9 @@ import os
 connect = sqlite3.connect("project.db")
 cursor = connect.cursor()
 
-clubs = cursor.execute("SELECT * FROM clubs")
+clubs = cursor.execute("SELECT name FROM clubs")
 club1 = clubs.fetchone()
+images = cursor.execute("SELECT logo FROM clubs")
 club1img = club1[2]
 img = Image.open("Club Logos/"+club1img)
 img.show()
