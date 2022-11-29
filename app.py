19fc17b0eb2ce9b9540399ba71b5af5a8f132483
jessjenk1ns <jessjenkins@college.harvard.edu>
2022-11-29 11:55:44 -0500

    commit

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import login_required
+
 # Configure application
 app = Flask(__name__)
 
